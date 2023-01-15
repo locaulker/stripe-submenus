4cf0547afc41e6a856e7d97a8bcf5bd066d41a98
Lawrence O. Caulker <locaulker@gmail.com>
2023-01-15 06:42:22 -0500

    added the hero structure

--- src/Hero.js
+++ src/Hero.js
@@ -3,10 +3,26 @@ import phoneImg from "./images/phone.svg"
 import { useGlobalContext } from "./context"
 
 const Hero = () => {
-  const data = useGlobalContext()
-  console.log(data)
+  const { closeSubmenu } = useGlobalContext()
 
-  return <h4>hero component</h4>
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payment Infrastructure for the Internet</h1>
+          <p>
+            Millions of companies of all sizes; from start-ups to Fortune 500s,
+            use the Stripe software and APIs to accept payments online, send
+            payouts, and manage their online businesses.
+          </p>
+          <button className="btn">Start Now!</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="Phone" />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
